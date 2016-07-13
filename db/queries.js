b05b3e28f20a57ebc109c8e3d33fe55d004b1a17
Lucy Follansbee <lfollansbee@gmail.com>
2016-07-12 18:59:48 -0600

    moving on to next story. working edit functionality

--- db/queries.js
+++ db/queries.js
@@ -15,8 +15,8 @@ module.exports = {
       cover_url: newBook.cover_url
     });
   },
-  getBookById: function(id){
-    return knex('book').select('book.id as book_id', 'book.*', 'genre.*').join('genre', 'genre.id', 'genre_id').where({'book.id':id})
+  getBookById: function(bookId){
+    return knex('book').select('book.id as book_id', 'book.*', 'genre.*').join('genre', 'genre.id', 'genre_id').where({'book.id':bookId})
   },
   deleteBook: function(bookId){
     return knex('book').where({id: bookId}).del()
@@ -27,5 +27,27 @@ module.exports = {
       description: bookUpdate.description,
       cover_url: bookUpdate.cover_url
     });
+  },
+  editBookAddGenre: function(newBook, bookId){
+    console.log(newBook);
+    knex('genre').select().where({'name': newBook.genre}).first()
+        .then(function(genre){
+          // console.log(genre);
+          if(genre){
+            return [genre]
+          }else{
+            return knex('genre').insert({name: newBook.genre}).returning('*')
+          }
+        })
+        .then(function(newGenre){
+          console.log(newGenre);
+          var book = {
+            title: newBook.title,
+            genre_id: newGenre[0].id,
+            description: newBook.description,
+            cover_url: newBook.cover_url
+          }
+          return knex('book').where({id:bookId}).update(book)
+        })//not working yet, adds new genre, but doesn't update book
   }
 }
